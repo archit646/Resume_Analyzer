868f963116ee5cfd1fc0b42d6d1dc9f115dc72c4
Archit Kumar <architk471@gmail.com>
2025-12-28 12:11:43 +0530

    testing

--- resume/serializers.py
+++ resume/serializers.py
@@ -4,4 +4,4 @@ class ResumeAnalysisSerializer(serializers.ModelSerializer):
     class Meta:
         model=ResumeAnalysis
         fields="__all__"
-        extra_kwargs={"resume":{"required":False}}
+        extra_kwargs={"resume":{"required":False},"detected_name":{"required":False},"detected_role":{"required":False},"experience_level":{"required":False},"match_score":{"required":False},"matched_skills":{"required":False},"missing_skills":{"required":False},"suggestions":{"required":False}}
